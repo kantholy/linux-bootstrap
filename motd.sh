b74863a2c391464f2b1ddc39d712d6a7ab18e3fe
Tobias <kantholy+github@gmail.com>
2015-11-17 12:21:27 +0100

    auto-download prefilled files

--- motd.sh
+++ motd.sh
@@ -6,8 +6,11 @@ sudo apt-get install figlet
 mkdir /etc/update-motd.d/
 # change to new directory
 cd /etc/update-motd.d/
-# create dynamic files
-touch 00-header && touch 10-sysinfo && touch 90-footer
+# download dynamic files
+wget https://raw.githubusercontent.com/kantholy/linux-bootstrap/master/motd/00-header
+wget https://raw.githubusercontent.com/kantholy/linux-bootstrap/master/motd/10-sysinfo
+wget https://raw.githubusercontent.com/kantholy/linux-bootstrap/master/motd/20-updates
+wget https://raw.githubusercontent.com/kantholy/linux-bootstrap/master/motd/90-footer
 # make files executable
 chmod +x /etc/update-motd.d/*
 # remove MOTD file
