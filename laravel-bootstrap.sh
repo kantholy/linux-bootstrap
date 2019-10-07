06fd8b3428d8f63215e62426cd4b399bab9207e8
Tobias <kantholy+github@gmail.com>
2019-10-07 21:09:09 +0200

    Update laravel-bootstrap.sh

--- laravel-bootstrap.sh
+++ laravel-bootstrap.sh
@@ -7,7 +7,8 @@ sudo add-apt-repository ppa:ondrej/php
 sudo apt update -qq
 
 # php
-sudo apt install -y php7.3 php7.3-bcmath php7.3-cli php7.3-curl php7.3-json php7.3-mbstring php7.3-xml php7.3-zip 
+sudo apt install -y php7.3-fpm php7.3-bcmath php7.3-cli php7.3-curl php7.3-json php7.3-mbstring php7.3-xml php7.3-zip
+sudo sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 256M/g' /etc/php/7.3/fpm/php.ini
 
 # more tools
 sudo apt install -y unzip git nginx mysql-server
@@ -23,3 +24,6 @@ echo 'export PATH="$HOME/.config/composer/vendor/bin:$PATH"' >> ~/.bashrc
 sudo debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password root"
 sudo debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password root"
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install mysql-server
+
+# laravel vhosts
+mkdir /www
