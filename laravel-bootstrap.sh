eaa3f78cb7490f87c21f5433c8f479007dc1796f
Tobias <kantholy+github@gmail.com>
2019-10-07 20:46:23 +0200

    Update laravel-bootstrap.sh

--- laravel-bootstrap.sh
+++ laravel-bootstrap.sh
@@ -7,7 +7,10 @@ sudo add-apt-repository ppa:ondrej/php
 sudo apt update
 
 # php
-sudo apt install php7.3 php7.3-bcmath php7.3-cli php7.3-curl php7.3-json php7.3-mbstring php7.3-xml php7.3-zip
+sudo apt install php7.3 php7.3-bcmath php7.3-cli php7.3-curl php7.3-json php7.3-mbstring php7.3-xml php7.3-zip 
+
+# more tools
+sudo apt install unzip git
 
 # composer
 curl -sS https://getcomposer.org/installer | php
